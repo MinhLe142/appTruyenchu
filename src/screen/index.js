bdb2b571b8c66191aadac8d28a1af9d80effe44c
Le Nhat Minh <lemint14120@gmail.com>
2023-12-27 01:30:52 +0700

    fix

--- src/screen/index.js
+++ src/screen/index.js
@@ -6,11 +6,6 @@ import {
 
 } from "react-native";
 import Login from "./login";
-<<<<<<< HEAD
-=======
-
-import register from "./register";
->>>>>>> 8b5a43e6a0ce1e75e9043717e3e71b03082d5401
 import Register from "./register";
 import Details from "./details";
 import {NavigationContainer} from '@react-navigation/native';
@@ -25,13 +20,9 @@ export default RootComponent = function () {
         <SafeAreaView>
         {/* <Login /> */}
         {/* <Register /> */}
-<<<<<<< HEAD
+
         <Details />
         </SafeAreaView>
     );
 }
-=======
-        </NavigationContainer>
 
-}
->>>>>>> 8b5a43e6a0ce1e75e9043717e3e71b03082d5401
